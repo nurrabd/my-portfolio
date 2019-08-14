fbfdf9f919dc9431e7985811da6b700a5095e830
nurhusein <nurabd@student.chalmers.se>
2019-08-14 21:21:50 +0200

    hej

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import nur from './nur.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h4> Welcome to my Page</h4>
+          <img
+          src="nur.jpg"
+          alt="hej"/>
           <h1> Under construction</h1>
-
+          <img
+          src="logo.svg"
+          alt="hej"
+          width="33.3333%"
+          height="33.3333%"
+          />
 
       </header>
     </div>
