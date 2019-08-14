6dd697a7868675e7ae27c0608a0cb3ffd3ed8963
nurhusein <nurabd@student.chalmers.se>
2019-08-14 21:12:11 +0200

    something

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={""} className="App-logo" alt="logo" />
+          <h1> Under construction</h1>
 
 
       </header>
