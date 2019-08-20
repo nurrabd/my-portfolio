382c13149f799b9b34206963f3a28d3410d14937
nurhusein <nurabd@student.chalmers.se>
2019-08-20 23:40:52 +0200

    rensa allt

--- src/App.js
+++ src/App.js
@@ -6,31 +6,22 @@ import {Container, Col, Row} from "react-bootstrap";
 import './App.css';
 
 function App() {
+
+    const cat1 = {}
+    const cat2 = "100"
+    const cat1ny = "300"
   return (
+
     <div className="App">
       <header className="App-header">
           <Container>
-              <Row>
-                  <Col className="col-6 border-primary" Style="border: outset" >
-                      <h1> Under construction</h1>
-                  </Col>
-                  <Col className="col-6 border-primary" Style="border: outset">
-                      <h1> Nurhusein Abdulkader</h1>
-                  </Col>
 
-
-              </Row>
           </Container>
 
 
 
 
-          <img
-          src={logo}
-          alt="hej"
-          width="33.3333%"
-          height="33.3333%"
-          />
+
 
       </header>
     </div>
