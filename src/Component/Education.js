d023976f2c05d70bfb20a32c69755763d1beef01
nurhusein <nurabd@student.chalmers.se>
2019-08-26 14:18:32 +0200

    ss

--- src/Component/Education.js
+++ src/Component/Education.js
@@ -26,10 +26,10 @@ class Education extends Component {
                             <div Style="border-left: 1px solid #9B9A9A; margin-left: 3px;  height: 190px;"></div>
                         </Col >
                         <Col className="col-11" Style="border:; text-align: left; margin-top: 10px; font-family: Myanmar MN; font-size: 17px; ">
-                            <h2>Bachelor thesis</h2>
-                            <h5>Front Edge IT</h5>
-                            <p>The goal of the  thesis was to create a cloud-based web application that generates a CV for  employee</p>
-                            <p>April 2019 – June 2019</p>
+                            <h2>Chalmers University of Technology, Gothenburg</h2>
+                            <h5>MASTER OF SCIENCE</h5>
+                            <p>Software Engineering and Technology</p>
+                            <p>September 2019 – Present</p>
                         </Col>
                     </Row>
 
@@ -38,10 +38,10 @@ class Education extends Component {
                             <div Style="border-left: 1px solid #9B9A9A; margin-left: 3px;  height: 190px;"></div>
                         </Col >
                         <Col className="col-11" Style="border:; text-align: left; margin-top: 10px; font-family: Myanmar MN; font-size: 17px; ">
-                            <h2 Style="color: #000000">Car fitter & Forklift driver</h2>
-                            <h5>Volvo Cars</h5>
-                            <p>Employed by the staffing company Lernia as a fitter at Volvo Cars. Tasks included high-speed work, forklift driving and team collaboration.</p>
-                            <p>June 2015 – Present</p>
+                            <h2 Style="color: #000000">Chalmers University of Technology, Gothenburgr</h2>
+                            <h5>Bachelor Degree, 180 H</h5>
+                            <p>Computer Engineering</p>
+                            <p>August 2016 – June 2019</p>
                         </Col>
                     </Row>
 
@@ -50,10 +50,10 @@ class Education extends Component {
                             <div Style="border-left: 1px solid #9B9A9A; margin-left: 3px;  height: 190px;"></div>
                         </Col >
                         <Col className="col-11" Style="border:; text-align: left; margin-top: 10px; font-family: Myanmar MN; font-size: 17px; ">
-                            <h2>Youth leader</h2>
-                            <h5>Göteborgs Stad</h5>
-                            <p>Provide an opportunity for social development and create a welcoming, fun and enthusiastic environment for youth</p>
-                            <p>January 2015 – June 2016</p>
+                            <h2>Hvitfeldtska gymnasiet, Gothenburg</h2>
+                            <h5>High school, 2500 points.</h5>
+                            <p>Natural Sciences</p>
+                            <p>August 2013 – June 2016</p>
                         </Col>
                     </Row>
                     <Row Style="border:">
