08e0b66d30062d7fc06978714093d9e9f93b914e
nurhusein <nurabd@student.chalmers.se>
2019-11-19 15:33:01 +0100

    hejsan

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,8 +5,8 @@ import {Link} from "react-scroll/modules";
 class Header extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" >
-                <Navbar.Brand href="/"  > <div className="fas fa-code fa-2x  border-danger" Style="width:"></div> </Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" className="back" >
+                <Navbar.Brand href="/"  > <div className="fas fa-code fa-2x  border-danger" Style="width:; padding-left:100px" ></div> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
 
--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -3,25 +3,25 @@ import React, {Component} from 'react';
 class LandingPage extends Component {
     render() {
         return (
-<div >
+<div className="back">
     {/** <img src="https://www.w3schools.com/css/paris.jpg"
      width="100%"
      height="700px"
      alt=""
      />**/}
 
-    <div Style="height:50px"></div>
+    <div Style="height:50px;" ></div>
             <div className="container" >
                 <p/>
 
                 <div className="row" >
-                    <div className="col-md-6 col-sm-12  border-danger" Style="height:320px; width:100%">
+                    <div className="col-md-6 col-sm-12  border-danger" Style="height:100%; width:100%">
                         <img
                             src={require('../images/pic1.JPG')}
                         alt="d"
                         className="img-responsive"
-                            height="320px"
-                            width="380px"
+                            height="100%"
+                            width="100%"
                             Style="border-radius:50%"
                        />
                     </div>
@@ -34,23 +34,23 @@ class LandingPage extends Component {
 
                 <div Style="height:60px"></div>
                 <div className="row">
-                    <div className="col-6  border-danger " Style="height:100px; width:100%; " align="right">
-                        <a className="fab fa-linkedin fa-5x" href="https://www.linkedin.com/in/nurhusein-abdulkader-185865112/"></a>
+                    <div className="col-6  border-danger " Style="height:100px; width:100% " align="right">
+                        <a className="fab fa-linkedin fa-5x" href="https://www.linkedin.com/in/nurhusein-abdulkader-185865112/"
+                           Style="color:blue"></a>
                     </div>
                     <div className="col-6    border-danger "  Style="height:100px; width:100%" align="left">
-                        <a className="fab fa-github fa-5x " href="https://github.com/nurrabd"></a>
+                        <a className="fab fa-github fa-5x " href="https://github.com/nurrabd"
+                           Style="color:black"></a>
                     </div>
                 </div>
                 <div Style="height:60px"></div>
-
                 <div className="row">
-
-                    <div className="col-2   border-danger fab fa-java fa-5x " Style="height:100px; width:100%"></div>
-                    <div className="col-2   border-danger fab fa-js fa-5x" Style="height:100px; width:100%"></div>
-                    <div className="col-2   border-danger fab fa-react fa-5x" Style="height:100px; width:100%"></div>
-                    <div className="col-2   border-danger fab fa-aws fa-5x" Style="height:100px; width:100%"></div>
-                    <div className="col-2   border-danger fab fa-html5 fa-5x" Style="height:100px; width:100%"></div>
-                    <div className="col-2   border-danger fab fa-css3-alt fa-5x" Style="height:100px; width:100%"></div>
+                    <div className="col-2   border-danger fab fa-java fa-5x " Style="height:100px; width:100%; color:pink"></div>
+                    <div className="col-2   border-danger fab fa-js fa-5x" Style="height:100px; width:100%; color:pink"></div>
+                    <div className="col-2   border-danger fab fa-react fa-5x" Style="height:100px; width:100%; color:pink"></div>
+                    <div className="col-2   border-danger fab fa-aws fa-5x" Style="height:100px; width:100%; color:pink"></div>
+                    <div className="col-2   border-danger fab fa-html5 fa-5x" Style="height:100px; width:100%; color:pink"></div>
+                    <div className="col-2   border-danger fab fa-css3-alt fa-5x" Style="height:100px; width:100%; color:pink"></div>
                 </div>
             </div>
 </div>
