7dbc8408704b71fc59d444163e27297f4a44e79f
nurhusein <nurabd@student.chalmers.se>
2020-10-04 00:00:20 +0200

    desc

--- src/page/App.js
+++ src/page/App.js
@@ -26,13 +26,23 @@ function App() {
                <h3 className="focus-in-contract-bck" Style="font-family:Roboto; "> WE MAKE IT HAPPEN</h3>
 
             </div>
-            <h5>website under construction</h5>
+            <h5></h5>
             {/** <Header/>
              <LandingPage/>
              <Portofolio/>
              <AboutMe/>
              <ContactMe/>**/}
 
+            <div className="typewriter">
+                <h1>website under construction</h1>
+            </div>
+
+            <h1>
+                <a href="" className="typewrite" data-period="2000"
+                   data-type='[ "Hi, Im Si.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+                    <span className="wrap"></span>
+                </a>
+            </h1>
 
 
         </div>
