e47752996744a20b9b65e9162877864bd9ee8608
nurhusein <nurabd@student.chalmers.se>
2022-10-16 11:44:36 +0200

    added a new repo

--- src/components/button/ButtonSearch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonSearch = () => {
-    return (
-        <button type="submit" className="btn btn-lg btn-before-dot btn-link border-0 p-0 min-w-auto link-no-space text-uppercase">Search</button>
-    );
-};
-
-export default ButtonSearch;
